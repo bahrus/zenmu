f5687434934f049d0a10d6b368a28d327eae278a
Bruce Anderson <anderson.bruce.b@gmail.com>
2016-12-18 13:24:18 -0500

    zen mu

--- zenCore.js
+++ zenCore.js
@@ -1,4 +1,5 @@
 "use strict";
+var numberDel = '###';
 function zen(strings) {
     var values = [];
     for (var _i = 1; _i < arguments.length; _i++) {
@@ -7,7 +8,10 @@ function zen(strings) {
     var sArr = strings;
     var sArrWithSiblings = [];
     for (var i = 0, ii = sArr.length; i < ii; i++) {
-        var sArrElement = sArr[i];
+        var word = sArr[i];
+        if (!word)
+            continue;
+        var sArrElement = word + '###' + i;
         if (sArrElement.substr(0, 1) === '+') {
             sArrWithSiblings[sArrWithSiblings.length - 1] += sArrElement;
         }
@@ -18,16 +22,12 @@ function zen(strings) {
     console.log(sArrWithSiblings);
     var outputArr = [];
     var tagBuffer = [];
-    var zenContext = {
-        closedFirstTag: false,
-        idx: 0,
-        openTag: null,
-    };
+    var zenContext = {};
     for (var _a = 0, sArrWithSiblings_1 = sArrWithSiblings; _a < sArrWithSiblings_1.length; _a++) {
         var tagSequence = sArrWithSiblings_1[_a];
         var tags = tagSequence.split('>');
         console.log(tags);
-        zenContext.closedFirstTag = false;
+        //zenContext.closedFirstTag = false;
         processTags(tags, outputArr, values, zenContext);
     }
     return outputArr;
@@ -38,17 +38,28 @@ function processTags(tags, outputArr, values, zenContext) {
         return;
     var surroundingTag = tags.shift();
     if (surroundingTag.indexOf('+') > -1) {
+        var siblingTags = surroundingTag.split('+');
+        for (var _i = 0, siblingTags_1 = siblingTags; _i < siblingTags_1.length; _i++) {
+            var tag = siblingTags_1[_i];
+            var tagWNumber = tag.split(numberDel);
+            outputArr.push('<' + tagWNumber[0] + '>');
+            if (tagWNumber.length > 0) {
+                var idx = parseInt(tagWNumber[1]);
+                outputArr.push(values[idx]);
+            }
+            outputArr.push('</' + tagWNumber[0] + '>');
+        }
     }
     else {
         outputArr.push("<" + surroundingTag + ">");
         processTags(tags, outputArr, values, zenContext);
-        if (!zenContext.closedFirstTag) {
-            if (zenContext.idx < values.length) {
-                outputArr.push(values[zenContext.idx]);
-                zenContext.idx++;
-            }
-            zenContext.closedFirstTag = true;
-        }
+        // if(!zenContext.closedFirstTag){
+        //     if(zenContext.idx < values.length){
+        //         outputArr.push(values[zenContext.idx]);
+        //         zenContext.idx++;
+        //     }
+        //     zenContext.closedFirstTag = true;
+        // }
         outputArr.push("</" + surroundingTag + ">");
     }
 }
--- zenCore.ts
+++ zenCore.ts
@@ -1,14 +1,18 @@
 
 interface ZenContext{
-    idx: number,
-    closedFirstTag: boolean,
-    openTag: string,
+    // idx: number,
+    // closedFirstTag: boolean,
+    // openTag: string,
 }
+
+const numberDel = '###';
 export function zen(strings : any, ...values){
     const sArr = strings as string[];
     const sArrWithSiblings = [];
     for(let i = 0, ii = sArr.length; i < ii; i++){
-        const sArrElement = sArr[i];
+        const word = sArr[i];
+        if(!word) continue;
+        const sArrElement = word + '###' + i;
         if(sArrElement.substr(0, 1) === '+'){
             sArrWithSiblings[sArrWithSiblings.length - 1] += sArrElement;
         }else{
@@ -19,14 +23,14 @@ export function zen(strings : any, ...values){
     const outputArr = [] as any[];
     const tagBuffer = [] as string[];
     const zenContext = {
-        closedFirstTag: false,
-        idx: 0,
-        openTag: null,
+        // closedFirstTag: false,
+        // idx: 0,
+        // openTag: null,
     } as ZenContext;
     for(const tagSequence of sArrWithSiblings){
         const tags = tagSequence.split('>');
         console.log(tags);
-        zenContext.closedFirstTag = false;
+        //zenContext.closedFirstTag = false;
         processTags(tags, outputArr, values, zenContext);
     }
     return outputArr;
@@ -36,17 +40,26 @@ function processTags(tags: string[], outputArr: any[], values, zenContext: ZenCo
     if(tags.length === 0) return;
     let surroundingTag = tags.shift();
     if(surroundingTag.indexOf('+') > -1){
-
-    }else{
-         outputArr.push(`<${surroundingTag}>`);
-        processTags(tags, outputArr, values, zenContext);
-        if(!zenContext.closedFirstTag){
-            if(zenContext.idx < values.length){
-                outputArr.push(values[zenContext.idx]);
-                zenContext.idx++;
+        const siblingTags = surroundingTag.split('+');
+        for(const tag of siblingTags){
+            const tagWNumber = tag.split(numberDel);
+            outputArr.push('<' + tagWNumber[0] + '>');
+            if(tagWNumber.length > 0){
+                const idx = parseInt(tagWNumber[1]);
+                outputArr.push(values[idx]);
             }
-            zenContext.closedFirstTag = true;
+            outputArr.push('</' + tagWNumber[0] + '>');
         }
+    }else{
+        outputArr.push(`<${surroundingTag}>`);
+        processTags(tags, outputArr, values, zenContext);
+        // if(!zenContext.closedFirstTag){
+        //     if(zenContext.idx < values.length){
+        //         outputArr.push(values[zenContext.idx]);
+        //         zenContext.idx++;
+        //     }
+        //     zenContext.closedFirstTag = true;
+        // }
         outputArr.push(`</${surroundingTag}>`);
     }
    
