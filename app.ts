53ba1e309c82610966bc082d96e6f40f95caf93e
Bruce Anderson <anderson.bruce.b@gmail.com>
2016-12-19 22:55:16 -0500

    zenmu

--- app.ts
+++ app.ts
@@ -2,4 +2,4 @@ import {zen} from './zenCore';
 
 const test1 = zen `div.myClass2>ul#myUL.myClass1>li${'Hello, '}+li${'World.'}`
 
-console.log(test1);
+console.assert(test1.join('') === `<div class="myClass2"><ul id="myUL" class="myClass1"><li>Hello, </li><li>World.</li></ul></div>`, 'test1 failed');
